8494ce7fd4c87e4cbf09b285c848e5ae2ed10b16
soaresmilton <eng.milton.soares@gmail.com>
2022-01-24 07:40:47 -0300

    App

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const puppeteer = require('puppeteer');
 
 async function wppMessage() {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
     await page.goto('https://web.whatsapp.com/');
 
-    // await page.waitForSelector('#app > div._1ADa8._3Nsgw.app-wrapper-web.font-fix');
-    await delay(5000);
+    await page.waitForSelector('.YtmXM');
+    await delay(3000);
 
     const contactName = 'Eu';
 
     await page.click(`span[title="${contactName}"]`);
 
-    await page.waitForSelector('.p3_M1');
+    await page.waitForSelector('.y8WcF');
 
     const messageBar = await page.$('div[tabindex="-1"]');
     await messageBar.focus();
